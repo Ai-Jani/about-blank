9ce5920a923e4417a600a97f9e3f23660849aefa
Ai-Jani <aino4.fi@gmail.com>
2025-06-14 17:49:31 +0900

    fix: typo (adopt cspell)

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,2 @@
-// `const obj = { key1: 46, key2: "moji" }` -> `ValuesOf<typeof obj>` -> `46 | "moji"`
+// `const obj = { key1: 46, key2: "curry" }` -> `ValuesOf<typeof obj>` -> `46 | "curry"`
 export type ValuesOf<T> = T[keyof T];
